515bac11f52af4507ded7a9ad55627cfbe02869e
JorritMontijn <jorritmontijn@gmail.com>
2020-01-29 12:57:47 +0100

    Update plotRaster.m

--- dependencies/plotRaster.m
+++ dependencies/plotRaster.m
@@ -1,19 +1,36 @@
-function plotRaster(vecSpikes,vecTrialStarts)
+function plotRaster(vecSpikes,vecTrialStarts,dblTrialDur,intPlotMaxSpikes)
 	%plotRaster Makes raster plot
-	%syntax: plotRaster(vecSpikes,vecTrialStarts)
+	%syntax: plotRaster(vecSpikes,vecTrialStarts,dblTrialDur,intPlotMaxSpikes)
 	%	input:
 	%	- vecSpikes; spike times (s)
 	%	- vecTrialStarts: trial start times (s)
+	%	- dblTrialDur: max trial dur
+	%	- intPlotMaxSpikes: maximum number of spikes to plot (default inf)
 	%
 	%Version history:
-	%1.0 - June 18 2019
+	%1.0 - 18 June 2019
 	%	Created by Jorrit Montijn
+	%1.1 - 2 Dec 2019
+	%	Added max spike number to plot [by JM]
+	
+	%get inputs
+	if ~exist('intPlotMaxSpikes','var') || isempty(intPlotMaxSpikes)
+		intPlotMaxSpikes = inf;
+	end
+	if ~exist('dblTrialDur','var') || isempty(dblTrialDur)
+		dblTrialDur = median(diff(vecTrialStarts));
+	end
+	
+	%subselect
+	if numel(vecSpikes) > intPlotMaxSpikes
+		vecSpikes = vecSpikes(sort(randperm(numel(vecSpikes),intPlotMaxSpikes)));
+	end
 	
 	%get spike times in trials
 	[vecTrialPerSpike,vecTimePerSpike] = getSpikesInTrial(vecSpikes,vecTrialStarts);
-	dblTrialDur = median(diff(vecTrialStarts));
 	
 	%plot per trial
+	cla;
 	hold all;
 	for intTrial=1:numel(vecTrialStarts)
 		vecTimes = vecTimePerSpike(vecTrialPerSpike==intTrial);
